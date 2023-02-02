05ee994ed828a60f7e4e120658823c2a089fcd98
Daniel Fros <danielfros97@gmail.com>
2023-02-02 10:10:54 -0300

    Se agregaron scrolls para los btn. del menú

--- main.js
+++ main.js
@@ -15,6 +15,32 @@ document.addEventListener('keydown',function(event){
       
 });
 
-
+// Getting current year
 var getTagYear = document.getElementById('currentYear');
  currentYear.innerHTML = new Date().getFullYear() + ". Daniel Fros";
+
+// get scrolled pixels
+  window.onscroll = function(e){
+    console.log(window.scrollY);
+  };
+
+
+
+
+// ------------ Scolling Menu ------------
+// About me
+document.getElementById('MeBtn').addEventListener('click',function(){
+  window.scrollTo({top: 500, behavior: 'smooth' })
+
+});
+// Skills 
+document.getElementById('mySkills').addEventListener('click', function(){
+  window.scrollTo({top: 1400, behavior: 'smooth'})
+});
+//Contact 
+document.getElementById('contact').addEventListener('click',function(){
+  window.scrollTo({
+    top: 1850,
+    behavior: 'smooth'
+  })
+});
