cb72609668aefbc5e4c47dd2f836f47535ceb1b2
Daniel Fros <danielfros97@gmail.com>
2023-02-03 11:04:00 -0300

    ajusta el scrolling cuando se cambia el tamaño

--- main.js
+++ main.js
@@ -19,28 +19,51 @@ document.addEventListener('keydown',function(event){
 var getTagYear = document.getElementById('currentYear');
  currentYear.innerHTML = new Date().getFullYear() + ". Daniel Fros";
 
-// get scrolled pixels
-  window.onscroll = function(e){
+// get scrolled pixels -- Enabled OPTIONAL
+
+  /* window.onscroll = function(e){
     console.log(window.scrollY);
-  };
+  }; */
 
 
+// get device min width
+window.onresize = function(){
+  console.log("Width in px: " + window.innerWidth);
+
+}
 
 
 // ------------ Scolling Menu ------------
+
+function SkillMenuResponsive(){
+  if(window.innerWidth >= 1115)
+    window.scrollTo({top: 1000, behavior: 'smooth'});
+  if(window.innerWidth >= 975)
+    window.scrollTo({top: 200, behavior: 'smooth'});
+    
+  if(window.innerWidth >= 892)
+    window.scrollTo({top: 1200, behavior: 'smooth'});
+  if(window.innerWidth >= 682)
+    window.scrollTo({top: 1350, behavior: 'smooth'});  
+
+}
+
+function ContactMenuResponsive(){
+  if(window.innerWidth >= 1115)
+    window.scrollTo({top: 1500,behavior: 'smooth'});
+  if(window.innerWidth >= 892)
+    window.scrollTo({top: 1600, behavior: 'smooth'});  
+  if(window.innerWidth >= 682)
+    window.scrollTo({top: 1850, behavior: 'smooth'}); 
+
+}
+
 // About me
 document.getElementById('MeBtn').addEventListener('click',function(){
-  window.scrollTo({top: 500, behavior: 'smooth' })
-
+  window.scrollTo({top: 560, behavior: 'smooth' })
 });
 // Skills 
-document.getElementById('mySkills').addEventListener('click', function(){
-  window.scrollTo({top: 1400, behavior: 'smooth'})
-});
+document.getElementById('mySkills').addEventListener('click',SkillMenuResponsive);
+
 //Contact 
-document.getElementById('contact').addEventListener('click',function(){
-  window.scrollTo({
-    top: 1850,
-    behavior: 'smooth'
-  })
-});
+document.getElementById('contact').addEventListener('click',ContactMenuResponsive);
